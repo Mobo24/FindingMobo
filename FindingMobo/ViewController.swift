752e7afd83e232d5c233299700ae02fadf505952
Mobo <princemoronfolu@gmail.com>
2020-01-30 22:28:51 -0600

    Initial Commit

---
+++ FindingMobo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindingMobo
+//
+//  Created by Mobolaji Moronfolu on 1/30/20.
+//  Copyright © 2020 Mobolaji Moronfolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
